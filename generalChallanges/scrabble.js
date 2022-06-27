9485eb03ebe719bf8687815d9868822e86a28c93
Rahaf <r.abualhassan@gmail.com>
2022-06-27 17:13:00 +0200

    refractured scrabble

--- generalChallanges/scrabble.js
+++ generalChallanges/scrabble.js
@@ -3,45 +3,37 @@ let tiles = "_x_tns"
 let word =  "cat"
 //points: 3 + 1 + 1
 
-tiles= "cat"
+//tiles= "cat"
 //word: cat
 //points: 1
 
 
-function check(tiles, word) {
-  
   const tileMap = {a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1, j: 8, k: 5, l: 1, m: 3, n: 1, o: 1, p: 3, q: 10, r: 1, s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10, _: 0};
-  
-  word = word.split("");
-  tiles = tiles.split("");  
+
+function check(tiles, word) {
+    
   let pointsObj = {};
-  
+  let points = 0;
   
   for (let i = 0; i < word.length; i++) {
-    for (let j = 0; j < tiles.length; j++) {
-        let keys = [];
-        if (tiles[j] === word[i]) {
-        keys.push(word[i]);
-        tiles = tiles.filter(e => e !== word[i]);
-        word = word.filter(e => e !== word[i]);
+        if (tiles.includes(word[i])) {
+          tiles = tiles.replace(word[i],'');
           
-          for (let key of keys)
-          if (key in tileMap) {
-            pointsObj[key] = tileMap[key];
+          if (word[i] in tileMap) {
+            pointsObj[word[i]] = tileMap[word[i]];
+            points += tileMap[word[i]]
           }
 	}
       
-      if (tiles[j] === "_" && tiles[j] in tileMap) {
-        pointsObj[tiles[j]] = tileMap[tiles[j]];
-        tiles[j] = word[i]
+      if (tiles.includes("_")) {
+        pointsObj["_"] = tileMap["_"];
       }
    
-    }
+    
   }
-  const points = Object.values(pointsObj).reduce((a, b) => a + b);
-  //console.log(pointsObj, points)
  return points
 }
 
 
-check(tiles, word)
+
+console.log(check(tiles, word))
